2ba5a2db1d894634388b7dfbe1798941d0c0a8f5
khiem <khiemnguyen.hye@gmail.com>
2026-05-26 21:44:54 +0700

    Scalfold

---
+++ apps/extension/src/extension.ts
@@ -0,0 +1,11 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  const signIn = vscode.commands.registerCommand("commma.signIn", async () => {
+    vscode.window.showInformationMessage("commma: sign-in not implemented yet");
+  });
+
+  context.subscriptions.push(signIn);
+}
+
+export function deactivate() {}
